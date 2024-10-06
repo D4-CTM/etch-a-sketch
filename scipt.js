d44705d7ed54726693eee03975fd4cd8d8dd2786
D4-CTM <josuedelcid325@gmail.com>
2024-10-06 17:53:14 -0600

    color chooser added

--- scipt.js
+++ scipt.js
@@ -2,7 +2,10 @@ const PIXEL_HEIGHT = "10px";
 const PIXEL_WIDTH = "10px";
 
 const dimensionsDiv = document.querySelector("#dimensions");
+const colorsDiv = document.querySelector("#colors");
+const colorOption = ["red", "blue", "green", "yellow", "cyan", "purple", "black", "white"];
 let sketchMatrix = [];
+let colorChoose;
 
 //We create the buttons to set the size of the sketch-box
 function setSizeButtons(dimensions) {
@@ -18,6 +21,23 @@ function setSizeButtons(dimensions) {
     setSizeButtons(dimensions + 16);
 }
 setSizeButtons(16);
+
+//Setter of the buttons that the user can choose to use on the sketch
+function setColorBoxes(index) {
+    if (index >= colorOption.length) return ;
+
+    const button = document.createElement("button");
+    button.style.backgroundColor = colorOption[index];    
+
+    button.addEventListener("click", () => {
+        colorChoose = button.style.backgroundColor;
+    });
+
+    colorsDiv.appendChild(button);
+
+    setColorBoxes(index + 1);
+}
+setColorBoxes(0);
 
 //setter for the sketches dimension
 function setDimension(dimensions) {
