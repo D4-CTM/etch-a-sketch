6b2f5b9802561b331de3f4544efc32333a1170d3
D4-CTM <josuedelcid325@gmail.com>
2024-10-06 17:30:08 -0600

    size definer done

--- scipt.js
+++ scipt.js
@@ -1,7 +1,35 @@
-const PIXEL_HEIGHT = 15;
-const PIXEL_WIDTH = 15;
-let sketchMatrix = [[]];
+const PIXEL_HEIGHT = "10px";
+const PIXEL_WIDTH = "10px";
 
-function setDimensions(width, height) {
-    if (width < 0 && height < 0) return ;
+const dimensionsDiv = document.querySelector("#dimensions");
+let sketchMatrix = [];
+
+//We create the buttons to set the size of the sketch-box
+function setSizeButtons(dimensions) {
+    if (dimensions > 100) return ;
+
+    const button = document.createElement("button");     
+    button.textContent = `${dimensions} x ${dimensions}`;
+    button.addEventListener("click", () => {
+        setDimension(dimensions);
+    });
+
+    dimensionsDiv.appendChild(button);
+    setSizeButtons(dimensions + 16);
 }
+setSizeButtons(16);
+
+//setter for the sketches dimension
+function setDimension(dimensions) {
+    sketchMatrix = [];
+    for (row = 0; row < dimensions; row++) {
+        sketchMatrix[row] = [];
+        for (col = 0; col < dimensions; col++) {
+            sketchMatrix[row][col] = document.createElement("div");
+
+
+        }
+    
+    }
+}
+setDimension(16);
