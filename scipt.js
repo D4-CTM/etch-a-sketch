3eb50c37a37d93a43097df8277d52994a24816ae
D4-CTM <josuedelcid325@gmail.com>
2024-10-06 23:21:39 -0600

    you can paint, indeed

--- scipt.js
+++ scipt.js
@@ -1,11 +1,21 @@
-const PIXEL_HEIGHT = "10px";
-const PIXEL_WIDTH = "10px";
-
+const sketchMatrixDiv = document.querySelector("#sketchMatrix");
+const colorPalette = document.getElementById("colorPalette");
 const dimensionsDiv = document.querySelector("#dimensions");
+const resetBTN = document.querySelector("#resetBTN");
+const randomBTN = document.querySelector("#randomBTN");
 const colorsDiv = document.querySelector("#colors");
-const colorOption = ["red", "blue", "green", "yellow", "cyan", "purple", "black", "white"];
-let sketchMatrix = [];
-let colorChoose;
+let randomOn = false;
+let dimensions;
+
+randomBTN.addEventListener("click", () => {
+    randomOn = !randomOn;
+
+    alert("Rainbow mode has been: " + (randomOn ? "activated" : "deactivated"));
+})
+
+resetBTN.addEventListener("click", () => {
+    setDimension(dimensions); 
+});
 
 //We create the buttons to set the size of the sketch-box
 function setSizeButtons(dimensions) {
@@ -22,34 +32,50 @@ function setSizeButtons(dimensions) {
 }
 setSizeButtons(16);
 
-//Setter of the buttons that the user can choose to use on the sketch
-function setColorBoxes(index) {
-    if (index >= colorOption.length) return ;
-
-    const button = document.createElement("button");
-    button.style.backgroundColor = colorOption[index];    
-
-    button.addEventListener("click", () => {
-        colorChoose = button.style.backgroundColor;
-    });
-
-    colorsDiv.appendChild(button);
-
-    setColorBoxes(index + 1);
-}
-setColorBoxes(0);
-
 //setter for the sketches dimension
-function setDimension(dimensions) {
-    sketchMatrix = [];
+function setDimension(_dimensions) {
+    dimensions = _dimensions;
+    clearSketchMatrixDiv();
+    const size = sketchMatrixDiv.clientHeight/dimensions;
+    const sketchMatrix = [];
     for (row = 0; row < dimensions; row++) {
-        sketchMatrix[row] = [];
+        sketchMatrix[row] = document.createElement("div");
         for (col = 0; col < dimensions; col++) {
-            sketchMatrix[row][col] = document.createElement("div");
+            const tile = document.createElement("div");
+            tile.setAttribute("class", "tile");
+            tile.style.backgroundColor = "white"
+            tile.style.height = size + "px";
+            tile.style.width = size + "px";
 
+            tile.addEventListener("mouseenter", () => {
+                if (randomOn) {
+                    tile.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+                } else colorSelected(tile);
+            });
 
+            sketchMatrix[row].appendChild(tile)
         }
-    
+        sketchMatrixDiv.appendChild(sketchMatrix[row]);
     }
 }
-setDimension(16);
+setDimension(8);
+
+function getRandomColor() {
+    return 1 + Math.floor(Math.random() * 100)%255;
+}
+
+function colorSelected(tile) {
+    colorChosen = colorPalette.value;
+
+    if (tile.style.background == colorChosen) {
+        tile.style.background = "white";
+    } else {
+        tile.style.background = colorChosen;
+    }
+}
+
+function clearSketchMatrixDiv() {
+    while (sketchMatrixDiv.firstChild) {
+        sketchMatrixDiv.removeChild(sketchMatrixDiv.firstChild);
+    }
+}
